fd936eaa26434c7779505d61cef2e5f66294a9a7
George <george.rotaru.pro@gmail.com>
2023-11-22 08:09:10 +0100

    test chart component

--- src/components/chart/Chart.test.tsx
+++ src/components/chart/Chart.test.tsx
@@ -1,22 +1,42 @@
 import React from "react";
-import {render} from "@testing-library/react";
-import {Chart} from "./Chart";
+import {render, screen} from "@testing-library/react";
+import {Chart} from "./Chart"; // Adjust the import path as necessary
+import "@testing-library/jest-dom";
 
-describe("Chart", () => {
+// Mocking `react-apexcharts`
+jest.mock("react-apexcharts", () => ({
+  __esModule: true,
+  default: ({options, series}) => (
+    <div role="chart">
+      <h2>{options.title.text}</h2>
+      <ul>
+        {series[0].data.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+    </div>
+  ),
+}));
+
+describe("Chart Component", () => {
   const mockProps = {
     labels: ["a", "b", "c", "d", "e", "f", "g"],
     data: [124, 12, 90, 85, 74, 90, 150],
     title: "Test Title",
   };
 
-  it("renders correctly", () => {
+  it("renders without crashing", () => {
     render(<Chart {...mockProps} />);
+  });
 
-    // Check if the title is rendered
-    // expect(screen.getByText(mockProps.title)).toBeInTheDocument();
+  it("renders the title correctly", () => {
+    render(<Chart {...mockProps} />);
+    expect(screen.getByText(mockProps.title)).toBeInTheDocument();
+  });
 
-    // Since we can't directly check the chart data or labels (as they are not directly rendered in the DOM),
-    // we check if the chart container is present
-    // expect(screen.getByRole("img", {name: /chart/i})).toBeInTheDocument();
+  it("renders the correct number of data points", () => {
+    render(<Chart {...mockProps} />);
+    const dataPoints = screen.getAllByRole("listitem");
+    expect(dataPoints.length).toBe(mockProps.data.length);
   });
 });
