850991fefa998acd002b9f4b055a928d7db3bf4b
George <george.rotaru.pro@gmail.com>
2023-11-22 06:07:12 +0100

    refact lint and make custom theme for paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { BaseLayout } from './components';
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import React from "react";
+import {ThemeProvider} from "@mui/material/styles";
+import {RouterProvider} from "react-router-dom";
+import {
+  QueryClient,
+  QueryErrorResetBoundary,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import {ErrorBoundary} from "react-error-boundary";
+import {Button} from "@mui/material";
+import {router} from "./routes";
+import theme from "./theme";
 
-const mdTheme = createTheme();
-
-function App() {
+const queryClient = new QueryClient();
+const App = () => {
   return (
-    <ThemeProvider theme={mdTheme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <BaseLayout><Outlet /></BaseLayout> }>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <QueryErrorResetBoundary>
+          {({reset}) => (
+            <ErrorBoundary
+              onReset={reset}
+              fallbackRender={({resetErrorBoundary}) => (
+                <div>
+                  There was an error!
+                  <Button
+                    onClick={() => {
+                      resetErrorBoundary();
+                    }}>
+                    Try again
+                  </Button>
+                </div>
+              )}>
+              <RouterProvider router={router} />
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
+      </QueryClientProvider>
     </ThemeProvider>
   );
-}
-
+};
 export default App;
